18c0cb55fece03346ba3986cbcc7d9690360795e
Tom Wells <tom@synthesis.co.za>
2024-09-02 21:42:38 +0200

    some cleans

--- 2015/app/Day01.hs
+++ 2015/app/Day01.hs
@@ -1,14 +1,14 @@
 module Day01 where
 
-import Text.Parsec (Parsec, many1, parse, string, (<|>))
+import Text.Parsec (Parsec, char, many, parse, (<|>))
 
 floors :: Parsec String () [Int]
-floors = many1 (string "(" *> pure 1 <|> string ")" *> pure (-1))
+floors = many $ char '(' *> pure 1 <|> char ')' *> pure (-1)
 
 part1 :: String -> Int
 part1 = either (error . show) sum . parse floors "input"
 
 part2 :: String -> Int
 part2 = either (error . show) basement . parse floors "input"
-  where
-    basement = (1 +) . length . takeWhile (/= -1) . scanl1 (+)
+ where
+  basement = length . takeWhile (/= -1) . scanl (+) 0
