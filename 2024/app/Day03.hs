c261d04713a34ed35de4e41985efd64617775661
Tom Wells <tom@synthesis.co.za>
2024-12-03 06:44:24 +0100

    cleans

--- 2024/app/Day03.hs
+++ 2024/app/Day03.hs
@@ -1,22 +1,16 @@
 module Day03 where
-import           Control.Applicative ((<|>))
-import           Data.Functor        (($>))
-import           Data.Maybe          (catMaybes)
-import           Handy
-import           Text.Parsec         (anyChar, char, choice, digit, many, many1,
-                                      string, try)
 
-data Instruction
-    = Mul Int Int
-    | Enable
-    | Disable
-    deriving (Show)
+import           Data.Maybe  (catMaybes)
+import           Handy
+import           Text.Parsec (anyChar, char, choice, digit, many1, string, try)
+
+data Instruction = Mul Int Int | Enable | Disable
 
 parser :: Parser [Instruction]
 parser = catMaybes <$> many1 (choice [ Just <$> try mul
                                      , Just <$> try enable
                                      , Just <$> try disable
-                                     , anyChar $> Nothing
+                                     , Nothing <$ anyChar
                                      ])
     where mul = Mul <$> (string "mul(" *> val <* char ',')
                     <*> val <* char ')'
@@ -27,16 +21,14 @@ parser = catMaybes <$> many1 (choice [ Just <$> try mul
 part1 :: IO Int
 part1 = do
     input <- parse parser <$> getInput Main 2024 3
-    let result = foldr (\i acc -> case i of
-                            Mul a b -> acc + a * b
-                            _       -> acc) 0 input
-    pure $ result
+    pure $ foldr (\i acc -> case i of
+                    Mul a b -> acc + a * b
+                    _       -> acc) 0 input
 
 part2 :: IO Int
 part2 = do
     input <- parse parser <$> getInput Main 2024 3
-    let result = foldl (\(on, acc) i -> case i of
+    pure $ snd $ foldl (\(on, acc) i -> case i of
                         Mul a b -> if on then (on, acc + a * b) else (on, acc)
                         Enable  -> (True, acc)
                         Disable -> (False, acc)) (True, 0) input
-    pure $ snd result
