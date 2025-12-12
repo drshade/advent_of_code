3f1eb98838d7c141e5ebab132ae3baf1f697149e
Tom Wells <tom@synthesis.co.za>
2025-12-12 13:56:38 +0100

    day12 cleans

--- 2025/app/Day12.hs
+++ 2025/app/Day12.hs
@@ -9,13 +9,13 @@ type Piece = [XY]
 
 input :: Parser' ([Piece], [(XY, [Int])])
 input = (,) <$> some (try piece) <*> some grid -- try because it could be a piece or a grid (both start with num)
-    where grid = (,) <$> ((,) <$> (num <* char 'x') <*> (num <* string ": "))
+  where grid = (,) <$> ((,) <$> (num <* char 'x') <*> (num <* string ": "))
                      <*> (num `sepBy` char ' ' <* newline)
-          piece = do
-            _id <- num <* char ':' <* newline
+        piece = do
+            void $ num <* char ':' <* newline -- ignore the "0:" (id) part
             offset <- xy -- get the starting point as an offset, and then grab exactly 3 lines
             join <$> sequenceA [line offset, line offset, line offset] <* newline
-          line offset = fmap (`subtractXY` offset) . catMaybes
+        line offset = fmap (`subtractXY` offset) . catMaybes
                         <$> some (choice [ Just <$> xy <* char '#'
                                          , Nothing <$ char '.' ]) <* newline
 
@@ -23,11 +23,7 @@ part1 :: IO Int
 part1 = do
     (pieces, trees) <- parse' input <$> puzzle Main 2025 12
     -- trivially reject all the trees which are too small to hold the total number of pieces,
-    -- even with perfect compression
-    let candidates = filter (\((sizex, sizey), targets) ->
+    -- even with perfect compression. Magically gets the right answer! (?? surprised but OK..)
+    pure $ length $ filter (\((sizex, sizey), targets) ->
                                 sizex * sizey > sum ((\(p, t) -> length p * t) <$> zip pieces targets)
                             ) trees
-    pure $ length candidates
-
-part2 :: IO ()
-part2 = pure () -- no part2!!
